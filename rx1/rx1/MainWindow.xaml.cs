be90e2ddb79b74ae798337d6ef28bf1a2a58d564
Rob Rodi <robert.rodi@gmail.com>
2013-07-01 14:38:40 -0700

    working via observer

--- rx1/rx1/MainWindow.xaml.cs
+++ rx1/rx1/MainWindow.xaml.cs
@@ -3,9 +3,11 @@
     using System;
     using System.ComponentModel;
     using System.Diagnostics;
+    using System.Reactive;
     using System.Reactive.Linq;
     using System.Runtime.CompilerServices;
     using System.Threading;
+    using System.Threading.Tasks;
     using System.Windows;
 
     using rx1.Annotations;
@@ -70,64 +72,116 @@
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    internal partial class MainWindow
-    {
+    partial class MainWindow
+    { 
+        public class GameState
+        {
+            private Timer timer;
+            private static readonly Func<string> RandomUser = () => Users[R.Next(Users.Length)];
+            public delegate void MagicEventHandler(object sender, MagicEvent e);
+            public event MagicEventHandler OnKill;
+
+            public GameState()
+            {
+                Kills = Observable.FromEventPattern<MagicEvent>(this, "OnKill").Select(e => e.EventArgs);
+            }
+            
+            private void GenerateAndFireKill()
+            {
+                if (this.OnKill != null)
+                    OnKill(this, GenerateEvent(DateTime.UtcNow.ToFileTime()));
+            }
+
+            public void Start(int rps)
+            {
+                timer = new Timer(_ => this.GenerateAndFireKill(), null, 0, 1000 / rps);
+            }
+            
+            private static MagicEvent GenerateEvent(long id)
+            {
+                generated++;
+                return new MagicEvent((int)id, RandomUser(), RandomUser());
+            }
+
+            public void Stop()
+            {
+                timer.Dispose();
+            }
+
+            protected virtual void OnOnKill(MagicEvent e)
+            {
+                var handler = this.OnKill;
+                if (handler != null)
+                {
+                    handler(this, e);
+                }
+            }
+
+            public IObservable<MagicEvent> Kills;
+        }
+
         //private readonly IObservable<MagicEvent> push;
         private static readonly Random R = new Random(12345);
         private static readonly string[] Users = new[] { "Rob", "Jeff", "Jason", "Mike", "Scott", "Eduard" };
         private static bool _isPushEnabled;
 
-        internal readonly StatsProcessor viewModel;
+        public readonly StatsProcessor viewModel;
+
+        public readonly GameState state = new GameState();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            var eventStream = Observable.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromMilliseconds(100)).Where(_ => _isPushEnabled).Select(i => GenerateEvent(i));
-            this.viewModel = new StatsProcessor(eventStream);
-            eventStream.Subscribe(viewModel);
+            //var eventStream = Observable.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromMilliseconds(5)).Where(_ => _isPushEnabled).Select(this.GenerateEvent);
+            this.viewModel = new StatsProcessor(state.Kills);
             this.DataContext = viewModel;
         }
 
         private void PubButton_Checked(object sender, RoutedEventArgs e)
         {
-            _isPushEnabled = !_isPushEnabled;
+            //_isPushEnabled = !_isPushEnabled;
+            uint value;
+            if (!uint.TryParse(EventsPerSecond.Text, out value))
+            {
+                MessageBox.Show("Please enter a valid number.");
+                return;
+            }
+
+            if (PubButton.IsChecked.IsTrue()) state.Start((int)value);
+            else state.Stop();
+
         }
 
         public static int generated;
-        public MagicEvent GenerateEvent(long id)
-        {
-            generated++;
-            Dispatcher.Invoke(() => { this.Generated.Content = generated.ToString(); });
-            Func<string> randomUser = () => Users[R.Next(Users.Length)];
-            return new MagicEvent((int)id, randomUser(), randomUser());
-        }
-
-        private void SubButton_Checked(object sender, RoutedEventArgs e)
-        {
-            //if (this.subscription != null)
-            //{
-            //    this.subscription.Dispose();
-            //    this.subscription = null;
-            //}
-
-            //if (this.SubButton.IsChecked.IsTrue() && this.push != null)
-            //{
-            //    //this.subscription = this.push.Subscribe(viewModel.ProcessEvent);
-            //}
-        }
     }
 
     public class StatsProcessor : INotifyPropertyChanged, IDisposable, IObserver<MagicEvent>
     {
+        private int _eventsPerSecond;
+
+        public int EventsPerSecond
+        {
+            get
+            {
+                return _eventsPerSecond;
+            }
+            set
+            {
+                if (_eventsPerSecond == value) return;
+
+                _eventsPerSecond = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("EventsPerSecond"));
+            }
+        }
         private readonly IObservable<MagicEvent> _eventStream;
 
         public StatsProcessor(IObservable<MagicEvent> eventStream)
         {
-            //this._eventStream = eventStream;
-            //this._eventStream.Subscribe(e => this.IncrementEvents());
-            //this._eventStream.Where(e => e.Killed != e.Killer).Subscribe(e => this.IncrementKills());
-            //this._eventStream.Where(e => e.Killed == e.Killer).Subscribe(e => this.IncrementSuicides());
+            this._eventStream = eventStream;
+            this._eventStream.Subscribe(e => this.IncrementEvents());
+            this._eventStream.Where(e => e.Killed != e.Killer).Subscribe(e => this.IncrementKills());
+            this._eventStream.Where(e => e.Killed == e.Killer).Subscribe(e => this.IncrementSuicides());
         }
 
         private void IncrementEvents()
@@ -138,6 +192,7 @@
         }
 
         private int _kills, _suicides, _events;
+
 
         private void IncrementKills()
         {
@@ -194,9 +249,9 @@
         }
     }
     
-    internal static class Extensions
+    public static class Extensions
     {
-        internal static bool IsTrue(this bool? value)
+        public static bool IsTrue(this bool? value)
         {
             return value.HasValue && value.Value;
         }
