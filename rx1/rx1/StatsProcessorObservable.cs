ae345b577d96311e33a855ac46428f7ae7dd9652
Rob Rodi <robert.rodi@gmail.com>
2013-07-01 16:44:39 -0700

    Works...

--- rx1/rx1/StatsProcessorObservable.cs
+++ rx1/rx1/StatsProcessorObservable.cs
@@ -5,24 +5,33 @@
     using System.Diagnostics;
     using System.Reactive.Linq;
 
+    public class Counter : Dictionary<int, int>
+    {
+        public Counter(IObservable<MagicEvent> eventStream, Func<MagicEvent, int> keySelector)
+        {
+            eventStream.GroupBy(keySelector).Subscribe(whatever => whatever.Scan(0, (i, @event) => i + 1).Do(kills => this[whatever.Key] = kills).Subscribe());
+        }
+    }
+
     public class StatsProcessorObservable : StatsProcessorBase, IDisposable
     {
         private readonly ICollection<IDisposable> subscriptions = new List<IDisposable>();
-        private readonly IDictionary<int, int> playerKillCounts = new Dictionary<int, int>();
-        private readonly IDictionary<int, int> playerDeathCounts = new Dictionary<int, int>();
+
+        private readonly IDictionary<int, int> playerKillCounts;
+        private readonly IDictionary<int, int> playerDeathCounts;
 
         public StatsProcessorObservable(IObservable<MagicEvent> eventStream)
             : base(eventStream)
         {
             this.EventStream.Subscribe(e => this.IncrementEvents());
+            
+            // Sum Kills
             this.EventStream.Where(e => e.Victim != e.Killer).Subscribe(e => this.IncrementKills());
             this.EventStream.Where(e => e.Victim == e.Killer).Subscribe(e => this.IncrementSuicides());
 
-            IObservable<IGroupedObservable<int, MagicEvent>> byKiller = this.EventStream.GroupBy(e => e.Killer);
-            byKiller.Subscribe(player => player.Scan(0, (i, @event) => i + 1).Do(kills => this.PlayerKillCounts[player.Key] = kills).Subscribe());
-
-            IObservable<IGroupedObservable<int, MagicEvent>> byVictim = this.EventStream.GroupBy(e => e.Victim);
-            byVictim.Subscribe(player => player.Scan(0, (i, @event) => i + 1).Do(kills => this.PlayerDeathCounts[player.Key] = kills).Subscribe());
+            // Sum Kills / player
+            playerKillCounts = new Counter(eventStream, e => e.Killer);
+            playerDeathCounts = new Counter(eventStream, e => e.Victim);
         }
 
         public IDictionary<int, int> PlayerKillCounts
